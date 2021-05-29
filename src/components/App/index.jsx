ec4373435538ccfb36e093ac71e30e3770d50f7b
Jitka Badhan Nebeska <jitkabadhan@gmail.com>
2021-05-29 10:19:28 +0200

    Router in App component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
@@ -6,7 +8,18 @@ import { Home } from '../Home';
 export const App = () => (
   <>
     <Header />
-    <Home />
+
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/reservation">
+          <h2>Detail jízdenky</h2>
+        </Route>
+      </Switch>
+    </Router>
+
     <Footer />
   </>
 );
